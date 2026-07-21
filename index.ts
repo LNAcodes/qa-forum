017fc8195bcca91dc2c2278bdbcf00748651ac42
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-07-21 14:48:41 +0200

    set up hono server on port 3000

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+const app = new Hono();
+
+app.get("/", (c) => c.text("Hono!"));
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
