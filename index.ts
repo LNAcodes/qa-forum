302ba431890c1e771cb3d7cacaa7306d6f5dcbce
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-07-21 15:50:49 +0200

    configure nunjucks as template engine, render index.html on GET /

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import { Hono } from "hono";
+import nunjucks from "nunjucks";
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hono!"));
+nunjucks.configure("views", {
+  autoescape: true,
+});
+
+app.get("/", (c) => {
+  const html = nunjucks.render("index.html"); // rendering to a string
+  return c.html(html);
+});
+// c.text("Hono!"));
 
 export default {
   port: 3000,
