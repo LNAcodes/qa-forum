6a1f81165812962a8dccd93751cf95e7d45de304
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-07-28 16:43:03 +0200

    refactor: rename template files from .html to .njk, update paths in index.ts and nodemon.json

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@ const app = new Hono();
 nunjucks.configure("views", {
   // securing: preventing XSS - Cross-Site-Scripting
   autoescape: true,
+  watch: true,
 });
 
 // Routes must be ordered from most specific to most dynamic.
@@ -18,7 +19,7 @@ nunjucks.configure("views", {
 // as an id parameter and the correct route is never reached.
 
 app.get("/", (c) => {
-  const html = nunjucks.render("index.html", { questions }); // rendering to a string
+  const html = nunjucks.render("index.njk", { questions }); // rendering to a string
   return c.html(html);
 });
 /* c.text("Hono!"));
@@ -30,7 +31,7 @@ app.get("/", (req, res) => {
 });
 */
 app.get("/questions/new", (c) => {
-  const html = nunjucks.render("new-question.html");
+  const html = nunjucks.render("new-question.njk");
   return c.html(html);
 });
 
@@ -43,7 +44,7 @@ app.get("/questions/search", async (c) => {
     question.title.toLowerCase().includes(searchQuery.toLowerCase()),
   );
 
-  const html = nunjucks.render("index.html", { questions: filteredQuestions });
+  const html = nunjucks.render("index.njk", { questions: filteredQuestions });
   return c.html(html);
 });
 
@@ -53,7 +54,7 @@ app.get("/questions/:id", (c) => {
     (question) => question.id === c.req.param("id"),
   );
 
-  const html = nunjucks.render("detail.html", { question: foundQuestion });
+  const html = nunjucks.render("detail.njk", { question: foundQuestion });
   return c.html(html);
 });
 
